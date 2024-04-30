5c71a607a1d8cb2cfb4cecce3175d5ef9e7bbb95
Egor Istomin <egoristomin@Egors-MacBook-Pro-2.local>
2024-04-30 17:21:55 -0400

    add

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <header> 
+      <h3>Egor's project1</h3>
+      <span>Time is here</span>
+    </header>
+
+    <main>
+      <h1>React.project1</h1>
+    </main>
+      
+    </>
+  )
+}
+
+export default App
